5d792f2f2d1046538378bc893845ca98dcbe23e8
sunjin092 <genie4898y@gmail.com>
2025-05-21 15:27:48 +0900

    final

--- src/pages/StepTwo.tsx
+++ src/pages/StepTwo.tsx
@@ -5,10 +5,10 @@ import skinData from '../data/data.json'; // JSON 임포트
 import './StepTwo.css';
 
 interface ZScoreAvg {
-  '색소침착 개수': number;
+  '색소침착': number;
   '수분': number;
   '탄력': number;
-  '모공 개수': number;
+  '모공': number;
 }
 
 interface PriorityConcern extends Array<string | number> {}
